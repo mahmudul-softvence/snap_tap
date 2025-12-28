b92a94d8287f88b4348602ed03650a7251730236
mahmudul-softvence <mahmudul@softvencedelta.com>
2025-12-28 16:39:57 +0600

    add image on the user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone')->nullable();
+            $table->string('image')->nullable();
             $table->string('facebook_id')->nullable();
             $table->string('google_id')->nullable();
             $table->string('github_id')->nullable();
