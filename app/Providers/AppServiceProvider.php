12a654ab5b6b1a7d19df65e4c8a62f1de22b86f8
mahmudul-softvence <mahmudul@softvencedelta.com>
2026-02-02 16:51:20 +0600

    rfgdfg

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -105,7 +105,6 @@ class AppServiceProvider extends ServiceProvider
 
         // -----------------------------
         // CHATGPT
-        Config::set('openai.api_key', Setting::get('chatgpt_api_key', env('OPENAI_API_KEY')));
 
 
         //CUSTOM MODELS
