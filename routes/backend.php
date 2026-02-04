879875c0e57fe38641e1df03e6c79c5529ff5199
sahadathossain96 <sahadat@softvencedelta.com>
2026-02-04 10:39:44 +0600

    some update

--- routes/backend.php
+++ routes/backend.php
@@ -33,39 +33,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('admin/customers/billing-history', [AdminSubscriptionController::class, 'allBillingHistory']);
 });
 
-
-//ADMIN PLAN AND SUBSCRIPTION.
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/adminplan', [AdminPlanController::class, 'index']);
-    Route::post('/plan/create', [AdminPlanController::class, 'store']);
-    Route::get('/admin-dashboard', [AdminSubscriptionController::class, 'adminDashboard']);
-    Route::get('/admin-subscription-dashboard', [AdminSubscriptionController::class, 'adminSubscriptionDashboard']);
-    Route::post('/admin/subscriptions/change-subscription', [AdminSubscriptionController::class, 'changeSubscription']);
-    Route::patch('/admin/subscriptions/change/{id}', [AdminSubscriptionController::class, 'changeStatus']);
-    Route::delete('/admin/subscriptions/delete/{id}', [AdminSubscriptionController::class, 'deleteSubscription']);
-    Route::get('admin/customers', [AdminSubscriptionController::class, 'getCustomerList']);
-    Route::get('admin/customers/subscription/{id}', [AdminSubscriptionController::class, 'customerSubscription']);
-    Route::get('admin/customers/billing-history/{id}', [AdminSubscriptionController::class, 'billingHistory']);
-    Route::get('admin/customers/billing-history', [AdminSubscriptionController::class, 'allBillingHistory']);
-});
-
-
-<<<<<<< HEAD
-    //user profile details
-    Route::middleware('role:super_admin','auth:sanctum')->group(function () {
-        Route::get('/admin/user-profile-show/{id}', [UserProfileManageController::class, 'userDetailsShow']);
-        Route::put('/admin/user-profile-Update/{id}', [UserProfileManageController::class, 'userDetailsUpdate']);
-        Route::get('/admin/user-profile-integration/{id}', [UserProfileManageController::class, 'userIntegrationDetails']);
-        Route::put('/admin/user-profile-integration-status-update/{id}', [UserProfileManageController::class, 'userIntegrationStatusUpdate']);
-=======
-
 //user profile details
 Route::middleware('role:super_admin', 'auth:sanctum')->group(function () {
     Route::get('/admin/user-profile-show/{id}', [UserProfileManageController::class, 'userDetailsShow']);
     Route::put('/admin/user-profile-Update/{id}', [UserProfileManageController::class, 'userDetailsUpdate']);
     Route::get('/admin/user-profile-integration/{id}', [UserProfileManageController::class, 'userIntegrationDetails']);
     Route::put('/admin/user-profile-integration-status-update/{id}', [UserProfileManageController::class, 'userIntegrationStatusUpdate']);
->>>>>>> b2d6c902226ef6c78a032236d482fdea810600a5
 
     Route::delete('/admin/user-business-account-remove/{id}', [UserProfileManageController::class, 'removeUserProviderAccount']);
 });
